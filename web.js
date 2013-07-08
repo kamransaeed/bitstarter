e3cd8cf80199408b6753375f5e2737fe08966a4c
Kamran Saeed <kamran.saeed@live.com>
2013-07-08 23:12:38 +0000

    fix web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-    var buf = fs.readFileSync('./bitstarter/index.html');
+    var buf = fs.readFileSync('index.html');
 
     response.send(buf.toString());
 });
